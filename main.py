7505d27d31b2ffc2ced76e8c839c3c8d8ce4aba8
kajugadaniels <kajugadaniels@gmail.com>
2025-10-30 22:08:54 +0200

    Setup modal for aws to run GPUs

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import modal
 
 app = modal.App("musify")
@@ -11,4 +12,20 @@ image = (
         "cd /tmp/ACE-Step && pip install ."
     ])
     .env({"HF_HOME": "/.cache/huggingface"})
+    .add_local_python_source("prompts")
 )
+
+model_volume = modal.Volume.from_name(
+    "ace-step-models", create_if_missing=True)
+hf_volume = modal.Volume.from_name("qwen-hf-cache", create_if_missing=True)
+
+music_gen_secrets = modal.Secret.from_name("music-gen-secret")
+
+@app.function(secrets=[modal.Secret.from_name("music-gen-secret")])
+def function_test():
+    print("The Modal setup is working correctly.")
+    print(os.environ["test"])
+
+@app.local_entrypoint()
+def main():
+    function_test.remote()
